d6e2346ce7680f5e4cb30a95dd5af3be44f1b7fb
nickvaningen <nickvaningen@hotmail.com>
2018-10-10 21:48:40 +0200

    the game
    de code

---
+++ Assets/ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+       
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Goball();
+        }
+    }
+    public void Goball()
+    {
+        float sx = Random.Range(2, 2) == 0 ? -1 : 1;
+        float sy = Random.Range(2, 2) == 0 ? -1 : 1;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(speed * sx, speed * sy, 0f);
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class movement : NetworkBehaviour
+{
+    public bool isplayer1;
+    public float speed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        if (isplayer1)
+        {
+            transform.Translate(0f, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0f);
+        }
+        else
+        {
+            transform.Translate(0f, Input.GetAxis("Vertical2") * speed * Time.deltaTime, 0f);
+        }
+        
+    }
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
